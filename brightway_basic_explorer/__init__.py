aa2996c086598fdaa9b57e6540675eaf3a88f27e
Benoît GSCHWIND <benoit.gschwind@mailbox.org>
2026-09-14 21:21:05 +0200

    Safer float management with params

--- brightway_basic_explorer/__init__.py
+++ brightway_basic_explorer/__init__.py
@@ -40,7 +40,9 @@ def activity_to_json_with_params(act, params):
         # Params provided ? Evaluate formulas
         if isinstance(amount, Basic):
             new_params = [(name, value) for name, value in _complete_and_expand_params(params, list(all_params().keys())).items()]
-            amount = float(amount.subs(new_params).evalf())
+            amount = amount.subs(new_params)
+            if amount.is_number:
+                amount = float(amount.evalf())
             de["computed_amount"] = True
 
         de["amount"] = amount
